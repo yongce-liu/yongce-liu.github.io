4a6ecfc5a7b33db7d33f60992f03fedb20cc8ba7
Yongce Liu <yongce.liu@outlook.com>
2024-07-21 17:49:35 +0800

    update

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,4 @@
-for file in images/*; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
-done
+# for file in images/*; do
+#     [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+# done
+convert "images/self.jpg"  -thumbnail 160x160 "tn/images/self.jpg"
