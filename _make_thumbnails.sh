4f374b72911b67dbe1503778b7ef9160f68ed19f
LIU Yongce <airrighttime@outlook.com>
2025-04-14 20:35:20 +0800

    Refactor code structure for improved readability and maintainability

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,4 +1,4 @@
 # for file in images/*; do
 #     [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
 # done
-convert "images/self.jpg"  -thumbnail 160x160 "tn/images/self.jpg"
+convert "images/rss2025.png"  -thumbnail 160x160 "tn/images/rss2025.png"
